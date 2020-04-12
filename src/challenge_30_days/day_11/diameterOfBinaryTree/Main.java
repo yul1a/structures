11e6b0ccbeb5bb0a0ff3705992a19a563a006417
Julia Teterleva <yulia.ekb@gmail.com>
2020-04-11 17:13:19 -0700

    30 days challenge day 11

--- src/challenge_30_days/day_11/diameterOfBinaryTree/Main.java
+++ src/challenge_30_days/day_11/diameterOfBinaryTree/Main.java
@@ -5,7 +5,12 @@ package challenge_30_days.day_11.diameterOfBinaryTree;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
+        System.out.println(s.diameterOfBinaryTree(TreeNode.buildTree(new int[]{1})) + " : expected 0");
+        System.out.println(s.diameterOfBinaryTree(TreeNode.buildTree(new int[]{1,2})) + " : expected 1");
         System.out.println(s.diameterOfBinaryTree(TreeNode.buildTree(new int[]{1,2,3,4,5})) + " : expected 3");
+        System.out.println(s.diameterOfBinaryTree(TreeNode.buildTree(new int[]{1,2,3,4,5,6,6,6})) + " : expected 5");
+        System.out.println(s.diameterOfBinaryTree(TreeNode.buildTree(new int[]{1,2,3,4,5,6,6,6,7,7,7,7,7,7,7,7,7,7}))
+                + " : expected 7");
 
     }
 
